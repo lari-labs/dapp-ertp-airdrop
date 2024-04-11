fdd00af4219880fa39d877770575d35429acacc2
Dan Connolly <dckc@madmode.com>
2024-04-10 21:08:11 -0500

    chore: prune interchain-ui/

--- interchain-ui/src/store/contract.ts
+++
@@ -1,8 +0,0 @@
-import { create } from 'zustand';
-
-interface ContractState {
-  instance?: unknown;
-  brands?: Record<string, unknown>;
-}
-
-export const useContractStore = create<ContractState>(() => ({}));
