7cdd833830786c7864b3d5d4fdc139cf87d2265f
Dan Connolly <dckc@madmode.com>
2024-05-02 17:28:26 -0500

    test: hash pubkey to address thousands of times on XS

--- contract/test/test-crypto-ava-xs.js
+++ contract/test/test-crypto-ava-xs.js
@@ -6,19 +6,40 @@
 import test from 'ava';
 import { pubkeyToAddress } from '../src/check-sig.js';
 
-test('pubkeyToAddress matches cosmjs tests', t => {
-  const cases = [
-    {
-      prefix: 'cosmos',
-      pubkey: {
-        type: 'tendermint/PubKeySecp256k1',
-        value: 'AtQaCqFnshaZQp6rIkvAPyzThvCvXSDO+9AzbxVErqJP',
-      },
-      expected: 'cosmos1h806c7khnvmjlywdrkdgk2vrayy2mmvf9rxk2r',
+const cases = [
+  {
+    prefix: 'cosmos',
+    pubkey: {
+      type: 'tendermint/PubKeySecp256k1',
+      value: 'AtQaCqFnshaZQp6rIkvAPyzThvCvXSDO+9AzbxVErqJP',
     },
-  ];
+    expected: 'cosmos1h806c7khnvmjlywdrkdgk2vrayy2mmvf9rxk2r',
+  },
+];
+
+test('pubkeyToAddress matches cosmjs tests', t => {
   for (const { prefix, pubkey, expected } of cases) {
     const actual = pubkeyToAddress(pubkey.value, prefix);
     t.is(actual, expected);
   }
+});
+
+/** @param {number} logn */
+const hashBench = logn => {
+  const [{ prefix, pubkey }] = cases;
+
+  const n = 1 << logn;
+  for (let i = 0; i < n; i += 1) {
+    pubkeyToAddress(pubkey.value, prefix);
+  }
+  return { size: n };
+};
+
+test('pubkeyToAddress performance', t => {
+  console.log('pubkeyToAddress performance start');
+  for (const n of [8, 12, 14]) {
+    hashBench(n);
+    console.log(n, 'end', 1 << n);
+  }
+  t.pass();
 });
