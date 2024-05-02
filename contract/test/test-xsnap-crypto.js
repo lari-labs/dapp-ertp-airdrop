7ad5be1c65d3880f9378d85062a2212874338825
Dan Connolly <dckc@madmode.com>
2024-05-02 15:21:09 -0500

    test: datastructure performance on XS

--- contract/test/test-xsnap-crypto.js
+++ contract/test/test-xsnap-crypto.js
@@ -90,3 +90,70 @@ test('high resolution timer', async t => {
   t.log({ milliseconds, date: new Date(milliseconds) });
   t.is(typeof milliseconds, 'number');
 });
+
+/** @param {number} logn */
+function dataStructurePerformance(logn) {
+  // eslint-disable-next-line no-bitwise
+  const n = 1 << logn;
+  const send = it => {
+    // eslint-disable-next-line no-undef
+    return issueCommand(new TextEncoder().encode(JSON.stringify(it)).buffer);
+  };
+  const t0 = performance.now();
+  for (let i = 0; i < 256; i += 1) {
+    const a = [];
+    for (let j = 0; j < n; j += 1) {
+      a.push(j);
+    }
+    const m = new Map();
+    for (let j = 0; j < n; j += 1) {
+      m.set(j, j);
+    }
+    for (let j = 0; j < n; j += 1) {
+      m.get(j);
+    }
+    const s = new Set();
+    for (let j = 0; j < n; j += 1) {
+      s.add(j);
+    }
+    for (let j = 0; j < n; j += 1) {
+      s.has(j);
+    }
+  }
+  const t1 = performance.now();
+  const dur = t1 - t0;
+  // O(n log(n))
+  const rate = (n * logn) / dur;
+  send({ size: n, dur, rate });
+}
+
+// This test fails intermittently due to some amount of noise that we cannot
+// completely eliminate.
+// Rather than have a very low-probability failing test, we skip this, but
+// retain the benchmark for future verification in the unlikely event that the
+// performance character of XS collections regresses.
+
+test('Array, Map, Set growth is O(log(n))', async t => {
+  const opts = options();
+  const vat = await xsnap({ ...opts, meteringLimit: 0 });
+  await vat.evaluate(
+    `globalThis.dataStructurePerformance = (${dataStructurePerformance})`,
+  );
+
+  const run = async size => {
+    const {
+      meterUsage: { compute },
+    } = await vat.evaluate(`dataStructurePerformance(${size})`);
+    // @ts-expect-error pop() may return undefined
+    const r = JSON.parse(opts.messages.pop());
+    t.log({ compute, r });
+    return { compute, r };
+  };
+
+  const { r: r1 } = await run(8);
+  const { r: r2 } = await run(10);
+  const { r: r3 } = await run(12);
+  t.log({ r2_1: r2.rate / r1.rate, r3_2: r3.rate / r2.rate });
+  t.true(r2.rate / r1.rate >= 1);
+  t.true(r3.rate / r2.rate >= 1);
+});
